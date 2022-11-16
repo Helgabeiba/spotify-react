2ef9ad051f79b05c52d876564b4673827cf91e6f
Helga Amelia Bergland <helgag13@uia.no>
2022-11-16 22:02:30 +0100

    login, hash token storage, logout,

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const CLIENT_ID = "664b7b95d83d4afb9360d23db26ad1c7"
+  const REDIRECT_URI = "http://localhost:3001"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState ("")
+
+
+  //getting the token 
+  useEffect( () => {
+    const hash = window.location.hash
+    let token = window.localStorage.getItem("token")
+
+    //getting the hash.. find access token and remove & on both ends..
+    if(!token && hash) {
+      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+    }
+
+    window.location.hash = ""
+    window.localStorage.setItem("token", token)
+    setToken(token)
+
+  }, [])
+
+  const logout = () => {
+    setToken("")
+    window.localStorage.removeItem("token")
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Sptyht</h1>
+        {!token ?
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login 
+            to Spotify</a>
+            : <button onClick= {logout}>Logout</button> }
       </header>
     </div>
   );
